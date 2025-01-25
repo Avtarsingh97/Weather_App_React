c3b69b72377eb2b595564f1fd310a698d9ae4b91
Avtar <officialavtar13@gmail.com>
2025-01-26 00:17:00 +0530

    update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,13 +13,19 @@ function App() {
   },[]);
 
   return (
-    <div className="app font-body">
-      <h1 className='text-5xl font-bold p-6 text-white'>Weather Forecast App</h1>
-      <Input className="border-1 rounded-xl px-2 py-2 w-96 text-lg m-2 text-white max-[520px]:w-50"/>
-      <Button onClick={weather.fetchData} value="Search" disabled={!weather.searchCity.trim()} />
-      <Card/>
+    <>
       
-    </div>
+      <div className="app font-body">
+        <h1 className='text-5xl font-bold p-6 text-white'>Weather Forecast App</h1>
+        <Input className="border-1 rounded-xl px-2 py-2 w-96 text-lg m-2 text-white max-[520px]:w-50"/>
+        <Button onClick={weather.fetchData} value="Search" disabled={!weather.searchCity.trim()} />
+        {(weather.data != null) ? (<Card/>):(
+        <div className='font-body text-4xl text-white'>
+          <p>Data Loading Please Wait....</p>
+        </div>)};
+      </div>
+      
+    </>
   );
 };
 
