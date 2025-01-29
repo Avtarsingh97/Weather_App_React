7f508677f71b6cdc3f13d8c91825027da5e8a228
Avtar <officialavtar13@gmail.com>
2025-01-29 15:58:39 +0530

    Add Clear Button

--- src/App.jsx
+++ src/App.jsx
@@ -18,11 +18,12 @@ function App() {
       <div className="app font-body">
         <h1 className='text-5xl font-bold p-6 text-white'>Weather Forecast App</h1>
         <Input className="border-1 rounded-xl px-2 py-2 w-96 text-lg m-2 text-white max-[520px]:w-50"/>
-        <Button onClick={weather.fetchData} value="Search" disabled={!weather.searchCity.trim()} />
+        <Button className="text-lg border-1 rounded-xl px-5 py-2 bg-blue-500 text-white hover:bg-blue-700 my-5 cursor-pointer" onClick={weather.fetchData} value="Search" disabled={!weather.searchCity.trim()} />
         {(weather.data != null) ? (<Card/>):(
         <div className='font-body text-4xl text-white'>
           <p>Data Loading Please Wait....</p>
         </div>)};
+        <Button className="text-lg border-1 rounded-xl px-5 py-2 bg-blue-500 text-white hover:bg-blue-700 my-5 cursor-pointer" onClick={weather.clearData} value="Clear" />
       </div>
       
     </>
